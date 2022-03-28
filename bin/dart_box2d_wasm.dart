e996d7b9d65186b5ce586a0a2bf27598beb4a5fd
Nick Meinhold <nick@enspyr.co>
2022-03-28 21:11:33 +1100

    Update to latest box2d.wasm and call a function

--- bin/dart_box2d_wasm.dart
+++ bin/dart_box2d_wasm.dart
@@ -5,13 +5,12 @@ import 'package:wasm/wasm.dart';
 void main(List<String> arguments) {
   final data = File('box2d.wasm').readAsBytesSync();
   final mod = WasmModule(data);
-  // File('out.text').writeAsStringSync(mod.describe());
+  File('out.text').writeAsStringSync(mod.describe());
 
   var instanceBuilder = mod.builder();
-  // instanceBuilder.addFunction(moduleName, name, fn)
   instanceBuilder.enableWasi();
   final instance = instanceBuilder.build();
-  // final setGravity =
-  // inst.lookupFunction('emscripten_bind_b2World_SetGravity_1');
-  // print(setGravity(0.0, -10.0));
+  final shape =
+  instance.lookupFunction('emscripten_bind_b2PolygonShape_b2PolygonShape_0');
+  print(shape());
 }
